f059cbfce46cb3976386743b96a1dbdbb42e593f
Lourenco Silva <julia.a.silva@capgemini.com>
2026-05-26 16:02:12 -0300

    even or odd numbers

---
+++ EvenOrOdd/EvenOrOdd/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace EvenOrOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("O número é par");
+            }
+            else
+            {
+                Console.WriteLine("O número é ímpar");
+            }
+        }
+    }
+}
