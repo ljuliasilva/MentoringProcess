38cfc73066d2b9e67d908a5dffe921054e35ae77
Lourenco Silva <julia.a.silva@capgemini.com>
2026-05-26 15:21:13 -0300

    welcome user program

---
+++ Welcome/Welcome/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Welcome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Boas vindas {0}!",name);
+        }
+    }
+}
