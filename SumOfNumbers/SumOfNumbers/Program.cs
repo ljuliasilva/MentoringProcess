42749fed1ec39078b9212018ea484b82df9d22f8
Lourenco Silva <julia.a.silva@capgemini.com>
2026-05-26 15:43:47 -0300

    sum of numbers

---
+++ SumOfNumbers/SumOfNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SumOfNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = num1 + num2;
+            Console.WriteLine("A soma dos números é: {0}", sum);
+        }
+    }
+}
