d7c532bf051a0d1e5d2d79889881c81c82f3fb2b
Lourenco Silva <julia.a.silva@capgemini.com>
2026-05-27 15:31:06 -0300

    average grade program

---
+++ AverageGrade/AverageGrade/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AverageGrade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a primeira nota: ");
+            double grade1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a segunda nota: ");
+            double grade2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a terceira nota: ");
+            double grade3 = double.Parse(Console.ReadLine());
+
+            double average = (grade1 + grade2 + grade3) / 3;
+
+            Console.WriteLine($"A média final é {average:F1}.");
+        }
+    }
+}
