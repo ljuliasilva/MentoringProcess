aa0daf7cd5014bd4959fe8430229f32504be6a5f
Lourenco Silva <julia.a.silva@capgemini.com>
2026-05-29 00:32:04 -0300

    temperature conversion program

---
+++ TemperatureConversion/TemperatureConversion/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TemperatureConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a temperatura em Celsius: ");
+            double celsius = double.Parse(Console.ReadLine());
+
+            double fahrenheit = (celsius * (1.8)) + 32;
+
+            Console.WriteLine($"A temperatura em Fahrenheit é {fahrenheit} °F.");
+        }
+    }
+}
