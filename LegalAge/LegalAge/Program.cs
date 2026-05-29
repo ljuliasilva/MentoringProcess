c08f6fe18cde72ed5c517aa2a35a55f648268049
Lourenco Silva <julia.a.silva@capgemini.com>
2026-05-28 23:43:03 -0300

    legal age program

---
+++ LegalAge/LegalAge/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LegalAge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a sua idade: ");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine("Você é maior de idade!");
+            }
+            else
+            {
+                Console.WriteLine("Você é menor de idade!");
+            }
+        }
+    }
+}
