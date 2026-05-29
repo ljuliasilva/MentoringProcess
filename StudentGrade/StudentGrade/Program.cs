8042e43ee86719f5c1da891be933e40198b994d2
Lourenco Silva <julia.a.silva@capgemini.com>
2026-05-29 00:06:40 -0300

    student grade program

---
+++ StudentGrade/StudentGrade/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace StudentGrade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a sua nota: ");
+            double grade = double.Parse(Console.ReadLine());
+
+            if (grade >= 7)
+            {
+                Console.WriteLine("Você foi aprovado.");
+            }
+            else if (grade <= 4)
+            {
+                Console.WriteLine("Você foi reprovado.");
+            }
+            else
+            {
+                Console.WriteLine("Você está de recuperação.");
+            }
+        }
+    }
+}
