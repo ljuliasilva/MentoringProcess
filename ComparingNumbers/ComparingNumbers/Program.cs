6d3e2f9b7f7f735eb6c4adff37f290b8e705a040
Lourenco Silva <julia.a.silva@capgemini.com>
2026-05-28 23:26:57 -0300

    comparing numbers program

---
+++ ComparingNumbers/ComparingNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ComparingNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine($"O número {num1} é o maior.");
+            }
+            else if (num1 < num2)
+            {
+                Console.WriteLine($"O número {num2} é o maior.");
+            }
+            else
+            {
+                Console.WriteLine("Os números são iguais.");
+            }
+        }
+    }
+}
