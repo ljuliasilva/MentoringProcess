3b783f4c3896718e978bd08632daeed47fd9a5ab
Lourenco Silva <julia.a.silva@capgemini.com>
2026-06-16 14:44:06 -0300

    countdown program

---
+++ CountDown/CountDown/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CountDown
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
