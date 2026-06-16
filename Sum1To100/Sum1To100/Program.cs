487bc0b67c757b13a6f69ea10a303e584c2cb062
Lourenco Silva <julia.a.silva@capgemini.com>
2026-06-16 14:55:59 -0300

    sum from 1 to 100

---
+++ Sum1To100/Sum1To100/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Sum1To100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine($"Soma de 1 até 100: {sum}");
+        }
+    }
+}
