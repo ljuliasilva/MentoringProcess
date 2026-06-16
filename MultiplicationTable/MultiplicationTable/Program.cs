4db586bf7e7c359ec829503cfe9851a130a68a84
Lourenco Silva <julia.a.silva@capgemini.com>
2026-06-16 14:38:08 -0300

    multiplication table program

---
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro: ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nTabuada do {num}: ");
+
+            for (int i = 1; i < 11; i++)
+            {
+                int result = num * i;
+                Console.WriteLine($"{num} x {i} = {result}");
+            }
+        }
+    }
+}
