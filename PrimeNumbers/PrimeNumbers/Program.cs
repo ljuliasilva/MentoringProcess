126d5b0115d4809c5f7a78f5f814af537e2d8c83
Lourenco Silva <julia.a.silva@capgemini.com>
2026-06-30 14:44:57 -0300

    prime numbers

---
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PrimeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            int number = int.Parse(Console.ReadLine());
+
+            bool prime = number >= 2;
+
+            for (int i = 2; i * i <= number && prime; i++) 
+            {
+                if (number % i == 0)
+                {
+                    prime = false;
+                }
+            }
+            Console.WriteLine(prime 
+                ? $"{number} é primo."
+                : $"{number} não é primo."); //só com booleanos
+        }
+    }
+}
