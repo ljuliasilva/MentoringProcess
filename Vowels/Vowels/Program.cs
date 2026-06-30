e81600160e95ffc2263d91b7f61bc424d11a6a47
Lourenco Silva <julia.a.silva@capgemini.com>
2026-06-30 14:28:30 -0300

    vowels program

---
+++ Vowels/Vowels/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Vowels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma palavra: ");
+            string word = Console.ReadLine()!.ToLower();
+            int wordQuantity = 0;
+
+            foreach (char letter in word)
+            {
+                if ("aeiou".Contains(letter))
+                {
+                    wordQuantity++;
+                }
+            }
+            Console.WriteLine($"A sua palavra possui {wordQuantity} vogais.");
+        }
+    }
+}
