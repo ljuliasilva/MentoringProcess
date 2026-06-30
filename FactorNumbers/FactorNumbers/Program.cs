ad8dd86d660d0723805b8b12ad0e8fe9cfb25874
Lourenco Silva <julia.a.silva@capgemini.com>
2026-06-30 15:21:21 -0300

    factor numbers

---
+++ FactorNumbers/FactorNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace FactorNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            int number = int.Parse(Console.ReadLine()!);
+
+            long factor = 1;
+
+            for (int i = 1; i <= number; i++)
+            {
+                factor *= i;
+            }
+
+            Console.WriteLine($"O resultado de {number}! é {factor}.");
+        }
+    }
+}
