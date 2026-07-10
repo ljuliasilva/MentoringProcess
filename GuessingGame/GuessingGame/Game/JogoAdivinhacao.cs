5fe42d4af4c98f307ce8ff4145290b278ede7a50
Lourenco Silva <julia.a.silva@capgemini.com>
2026-07-10 13:06:49 -0300

    guessing game program

---
+++ GuessingGame/GuessingGame/Game/JogoAdivinhacao.cs
@@ -0,0 +1,30 @@
+﻿namespace GuessingGame.Game
+{
+    public class JogoAdivinhacao
+    {
+        private readonly int numeroSecreto;
+        private int tentativas;
+        private readonly Random aleatorio;
+
+        public JogoAdivinhacao()
+        {
+            aleatorio = new Random();
+            numeroSecreto = aleatorio.Next(1, 101);
+            tentativas = 0;
+        }
+
+        public void Iniciar()
+        {
+            Console.WriteLine("==================================");
+            Console.WriteLine("      JOGO DA ADIVINHAÇÃO");
+            Console.WriteLine("==================================");
+            Console.WriteLine("Dê o seu palpite de 1 a 100: ");
+
+            bool acertou = false;
+
+            while (acertou)
+            {
+            }
+        }
+    }
+}
---
+++ GuessingGame/GuessingGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using GuessingGame.Game;
+
+namespace GuessingGame
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            JogoAdivinhacao game = new JogoAdivinhacao();
+            game.Iniciar();
+            
+        }
+    }
+}
