44da0980b8230b9ae296fb04d4533e01b0ce7f4c
Lourenco Silva <julia.a.silva@capgemini.com>
2026-07-22 14:18:06 -0300

    menu program

---
+++ Menu/Menu/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Menu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Menu: ");
+            Console.WriteLine("1- Olá! ");
+            Console.WriteLine("2- Data atual: ");
+            Console.WriteLine("3- Sair");
+            Console.WriteLine("Escolha uma opção: ");
+
+            int option = int.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Olá!");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Data atual: " + DateAndTime.Now.ToShortDateString());
+                    break;
+
+                case 3:
+                    Console.WriteLine("Saindo...");
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida");
+                    break;
+            }
+
+        }
+    }
+}
